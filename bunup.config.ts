e9d84bb8b45b803f23433e2651643e455015bda5
arshad <arshadpyaseen@gmail.com>
2025-08-11 22:26:31 +0530

    chore: update

--- bunup.config.ts
+++ bunup.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'bunup'
+import { exports, unused } from 'bunup/plugins'
 
 export default defineConfig({
-	entry: ['bunup.config.ts'],
+	entry: ['src/index.ts'],
 	format: ['esm', 'cjs'],
+	plugins: [exports(), unused()],
 })
